387624165199b1eb76ab522bbfa6d0f880e9a41a
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2025-09-26 08:08:07 -0400

    further fixing build system

--- scripts/build_linux_app.sh
+++ scripts/build_linux_app.sh
@@ -28,7 +28,7 @@ then
 fi
 
 cd /tmp/simpleiptv
-${cmake_exe} --workflow --preset=${CMAKE_PRESET}
+${cmake_exe} -DCPACK_DISTRIBUTION=${distro} --workflow --preset=${CMAKE_PRESET}
 
 mkdir -p /tmp/simpleiptv/packages
 cp /tmp/build/simpleiptv-${SIMPLEIPTV_VERSION}-${distro}.* /tmp/simpleiptv/packages/
