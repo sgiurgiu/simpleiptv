e55981031897617a8104262b0475b58cf1d8f76e
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2025-09-26 21:53:53 -0400

    Added first steps towards appimage. Fixed some more bugs with build system in general

--- scripts/build_linux_app.sh
+++ scripts/build_linux_app.sh
@@ -8,6 +8,7 @@ distro=""
 if [ -n "$1" ]
 then
     distro=$1
+    package_name="simpleiptv-${SIMPLEIPTV_VERSION}-${distro}"
     if [[ "${distro}" == "ubuntu" || "${distro}" == "debian" ]]
     then
         echo "Building a deb package"
@@ -17,6 +18,12 @@ then
     then
         echo "Building an rpm package"
         CMAKE_PRESET="release"
+    fi
+    if [[ "${distro}" == "appimage" ]]
+    then
+        echo "Building an appimage package"
+        package_name="simpleiptv-${SIMPLEIPTV_VERSION}.AppImage"
+        CMAKE_PRESET="release-tgz"
     fi
 fi
 
@@ -29,8 +36,18 @@ fi
 
 cd /tmp/simpleiptv
 export DISTRIBUTION=${distro}
-
+rm -rf build-release
 ${cmake_exe}  --workflow --preset=${CMAKE_PRESET}
 
 mkdir -p /tmp/simpleiptv/packages
-cp /tmp/build/simpleiptv-${SIMPLEIPTV_VERSION}-${distro}.* /tmp/simpleiptv/packages/
+if [[ "${distro}" == "appimage" ]]
+then
+    export NO_STRIP=true
+    rm -rf /tmp/simpleiptv/AppDir
+    mkdir -p /tmp/simpleiptv/AppDir/usr
+    tar -xvf /tmp/simpleiptv/build-release/simpleiptv-${SIMPLEIPTV_VERSION}-${distro}.tar.gz -C /tmp/simpleiptv/AppDir/usr
+    cp -f /tmp/simpleiptv/simpleiptv_appimage.desktop /tmp/simpleiptv/AppDir/usr/share/applications/simpleiptv.desktop
+    /opt/appimagetool-x86_64.AppImage -s deploy ./AppDir/usr/share/applications/simpleiptv.desktop
+else
+    cp /tmp/simpleiptv/build-release/${package_name}.* /tmp/simpleiptv/packages/
+fi
