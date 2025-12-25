3228096f1e0529ebacb5fa6882219b04903976e8
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2025-12-24 21:59:25 -0500

    Trying to enable repo publishing for simpleiptv as well

--- scripts/build_linux_app.sh
+++ scripts/build_linux_app.sh
@@ -2,27 +2,39 @@
 
 set -x
 
+cd /tmp/simpleiptv
 export VCPKG_ROOT=/opt/vcpkg
 CMAKE_PRESET=""
 distro=""
 if [ -n "$1" ]
 then
     distro=$1
-    package_name="simpleiptv-${SIMPLEIPTV_VERSION}-${distro}"
     if [[ "${distro}" == "ubuntu" || "${distro}" == "debian" ]]
     then
         echo "Building a deb package"
         CMAKE_PRESET="release-deb"
+        export OS_FAMILY=${distro}
+        export OS_VERSION=$( . /etc/os-release && echo "$VERSION_ID" )
+        export OS_DIST_TAG="deb${OS_VERSION}"
+        if [[ "${distro}" == "ubuntu" ]]
+        then
+            export OS_DIST_TAG="ubuntu${OS_VERSION}"
+        fi
     fi
-    if [[ "${distro}" == "fedora" || "${distro}" == "redhat" || "${distro}" == "centos" || "${distro}" == "suse" ]]
+    if [[ "${distro}" == "fedora" ]]
     then
         echo "Building an rpm package"
         CMAKE_PRESET="release"
+        export OS_FAMILY=fedora
+        export OS_VERSION=$(rpm --eval '%{fedora}')
+        export OS_DIST_TAG="fc${OS_VERSION}"
+        echo "OS_VERSION=${OS_VERSION}" > build.env
+        echo "OS_FAMILY=${OS_FAMILY}" >> build.env
+        echo "OS_DIST_TAG=${OS_DIST_TAG}" >> build.env
     fi
     if [[ "${distro}" == "appimage" ]]
     then
         echo "Building an appimage package"
-        package_name="simpleiptv-${SIMPLEIPTV_VERSION}.AppImage"
         CMAKE_PRESET="release-tgz"
     fi
 fi
@@ -34,8 +46,6 @@ then
     cmake_exe="cmake"
 fi
 
-cd /tmp/simpleiptv
-export DISTRIBUTION=${distro}
 rm -rf build-release
 ${cmake_exe}  --workflow --preset=${CMAKE_PRESET}
 
@@ -45,10 +55,10 @@ then
     export NO_STRIP=true
     rm -rf /tmp/simpleiptv/AppDir
     mkdir -p /tmp/simpleiptv/AppDir/usr
-    tar -xvf /tmp/simpleiptv/build-release/simpleiptv-${SIMPLEIPTV_VERSION}-${distro}.tar.gz -C /tmp/simpleiptv/AppDir/usr
+    tar -xvf /tmp/simpleiptv/build-release/simpleiptv-${SIMPLEIPTV_VERSION}*.tar.gz -C /tmp/simpleiptv/AppDir/usr
     cp -f /tmp/simpleiptv/simpleiptv_appimage.desktop /tmp/simpleiptv/AppDir/usr/share/applications/simpleiptv.desktop
     /opt/linuxdeployqt-continuous-x86_64.AppImage ./AppDir/usr/share/applications/simpleiptv.desktop -appimage
     cp SimpleIPTV*.AppImage /tmp/simpleiptv/packages/
 else
-    cp /tmp/simpleiptv/build-release/${package_name}.* /tmp/simpleiptv/packages/
+    cp /tmp/simpleiptv/build-release/simpleiptv* /tmp/simpleiptv/packages/
 fi
